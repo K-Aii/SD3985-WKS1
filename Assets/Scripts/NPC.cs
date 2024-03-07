8204825c8d96a5e257cb243bdf4b96487f6571b0
K-Aii <86560772+K-Aii@users.noreply.github.com>
2024-03-07 18:31:00 +0800

    20240307-1830

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public float displayTime = 4.0f;
+    public GameObject dialogueBox;
+    float timerDisplay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -60,6 +60,13 @@ public class RubyController : MonoBehaviour
             Launch();
         }
 
+        if (Input.GetKeyDown(KeyCode.X)) {
+            RaycastHit2D hit = Physics2D.Raycast(rb.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null) {
+                Debug.Log("Raycast hit: "+hit.collider.gameObject);
+            }
+        }
+
     }
 
     private void FixedUpdate()
